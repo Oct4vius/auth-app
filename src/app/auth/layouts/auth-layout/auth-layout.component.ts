25298dfd3ee9112ce7ae08d2f599f8a1f0c720e3
Octavio Luperón <armando2445@outlook.com>
2024-05-06 09:16:12 -0400

    add: folder distribution

---
+++ src/app/auth/layouts/auth-layout/auth-layout.component.ts
@@ -0,0 +1,8 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './auth-layout.component.html',
+  styleUrl: './auth-layout.component.scss',
+})
+export class AuthLayoutComponent { }
---
+++ src/app/dashboard/layouts/dashboard-layout/dashboard-layout.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './dashboard-layout.component.html',
+  styleUrl: './dashboard-layout.component.scss',
+})
+export class DashboardLayoutComponent { }
